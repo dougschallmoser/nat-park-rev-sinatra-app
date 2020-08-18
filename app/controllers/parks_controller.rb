3a2b8a9b207bc73bcf24ca06ec7413239ee1222b
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-17 18:51:15 -0700

    updates show page for specific parks

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,4 +4,9 @@ class ParksController < ApplicationController
 
     use Rack::Flash
 
+    get '/parks/:slug' do 
+        @park = Park.find_by_slug(params[:slug])
+        erb :"parks/show"
+    end
+
 end
--- app/models/park.rb
+++ app/models/park.rb
@@ -3,5 +3,13 @@ class Park < ActiveRecord::Base
     has_many :posts
     has_many :users, through: :posts
     belongs_to :state
+
+    def slug
+        self.name.downcase.strip.gsub(' ', '-')
+    end
+
+    def self.find_by_slug(slug_name)
+        self.all.detect {|park| park.slug == slug_name}
+    end 
     
 end
