ec337e5f3ea9a14eae2282ea79e7554ea3da8e84
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-18 15:10:11 -0700

    updates new post render file with form fields

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,15 @@ class PostsController < ApplicationController
         @posts = Post.all
         erb :"posts/index"
     end 
-
+    
+    get '/posts/new' do 
+        erb :"posts/new"
+    end
+    
     get '/posts/:id' do 
         @post = Post.find(params[:id])
         erb :"posts/show"
     end
 
-    get '/posts/new' do 
-        erb :"posts/new"
-    end
 
 end
