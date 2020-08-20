9ce82b80fbff28c9b190fb8ef8f1f73f7872a440
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-20 15:16:12 -0700

    updates error handling and checking for state names

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -5,14 +5,22 @@ class ParksController < ApplicationController
     use Rack::Flash
 
     get '/parks/:slug' do 
-        @park = Park.find_by_slug(params[:slug])
-        erb :"parks/show"
+        if @park = Park.find_by_slug(params[:slug])
+            erb :"parks/show"
+        else
+            erb :"parks/error"
+        end
     end
 
     get '/parks/states/:slug' do 
-        @state_name = params[:slug].split("-").map(&:capitalize).join(" ")
-        @posts = Post.find_by_slug_state(params[:slug])
-        erb :"parks/show_states"
+        @states_unique = Park.all.collect{|park| park.state}.uniq
+        if @states_unique.detect {|state| state.downcase.strip.gsub(' ', '-') == params[:slug]}
+            @state_name = params[:slug].split("-").map(&:capitalize).join(" ")
+            @posts = Post.find_by_slug_state(params[:slug])
+            erb :"parks/show_states"
+        else
+            erb :"parks/error"
+        end
     end
 
 end
