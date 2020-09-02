11721eccb534ca2da105e4a04c339ede02887281
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-01 22:22:24 -0700

    remove call to Park.all

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,6 +1,6 @@
 
 class ParksController < ApplicationController
-
+    
     get '/parks/:park_name' do 
         if @park = Park.find_by_slug(params[:park_name])
             erb :"parks/show_parks"
@@ -9,12 +9,12 @@ class ParksController < ApplicationController
             redirect "/posts"
         end
     end
-
+    
     get '/states/:state_name' do 
         unique_states = Park.distinct.pluck(:state)
         if unique_states.detect {|state| state.downcase.strip.gsub(' ', '-') == params[:state_name]}
             @state_name = params[:state_name].split("-").map(&:capitalize).join(" ")
-            @parks_in_state = Park.all.where(:state => @state_name)
+            @parks_in_state = Park.where(:state => @state_name)
             @posts = Post.find_by_slug_state(params[:state_name])
             erb :"parks/show_states"
         else
